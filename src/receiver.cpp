44337b102db4bfa03df8fd30f363489786ceaf19
Andrei <aformusatii@gmail.com>
2016-01-04 21:05:22 +0200

    Fixed receive buffer issue.

--- src/receiver.cpp
+++ src/receiver.cpp
@@ -8,80 +8,68 @@
 
 #include "../nrf24l01/RF24.h"
 
-volatile int listen_enabled = 0;
+volatile int irq_enabled = 0;
 RF24 radio;
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
-void loop();
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
 
-  printf("Start NRF24L01P test...\n");
+    printf("Start NRF24L01P test...\n");
 
-  radio.begin();
-  radio.setRetries(15,15);
-  radio.setPayloadSize(8);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setChannel(110);
+    radio.begin();
+    radio.setRetries(15, 15);
+    radio.setPayloadSize(8);
+    radio.setPALevel(RF24_PA_MAX);
+    radio.setChannel(110);
 
-  radio.openWritingPipe(pipes[0]);
-  radio.openReadingPipe(1,pipes[1]);
+    radio.openWritingPipe(pipes[0]);
+    radio.openReadingPipe(1, pipes[1]);
 
-  radio.startListening();
+    radio.startListening();
 
-  radio.printDetails();
+    radio.printDetails();
 
-  listen_enabled = 1;
+    irq_enabled = 1;
 
-  fflush(stdout);
+    fflush(stdout);
 
-  while (1) {
-	  //loop();
-     sleep(5);
-  }
-
-  return 0;
-}
-
-void loop() {
-    while (!radio.available()) {
+    while (1) {
+        sleep(5);
     }
 
-	uint8_t data[] = {0, 0};
-    radio.read(data, 2);
-
-    // Spew it
-    //printf("Data: %i, %i\n\r", data[0], data[1]);
-    int16_t temp_int_rec = (int16_t) (((data[0] & 0x00FF) << 8) | (data[1] & 0x00FF));
-    printf("\n temp_int_rec=%d", temp_int_rec);
+    return 0;
 }
 
 void dataReceivedIRQ() {
-    if (!listen_enabled) {
-    	return;
+    if (!irq_enabled) {
+        return;
     }
 
     bool tx_ok, tx_fail, rx_ok;
-    radio.whatHappened(tx_ok,tx_fail,rx_ok);
+    radio.whatHappened(tx_ok, tx_fail, rx_ok);
 
     printf("\nIRQ!");
 
-    //while (!radio.available()) {
-    //}
-
     if (rx_ok) {
-		uint8_t data[] = {0, 0, 0, 0};
-		radio.read(data, 4);
+        uint8_t data[] = { 0, 0, 0, 0 };
+        bool has_more_data = radio.read(data, 4);
 
-		if (data[0] == 100) {
-                    // Spew it
-                    int16_t temp_int_rec = (int16_t) (((data[2] & 0x00FF) << 8) | (data[3] & 0x00FF));
-                    printf("\n temp_int_rec=%d", temp_int_rec);
+        if (data[0] == 100) {
+            // Spew it
+            int16_t temp_int_rec = (int16_t) (((data[2] & 0x00FF) << 8)
+                    | (data[3] & 0x00FF));
+            printf("\nT=%d", temp_int_rec);
 
-                    char buffer[255];
-                    sprintf(buffer, "./save.sh %d %d", data[1], temp_int_rec);
-                    system(buffer);
-		}
+            char cmd[255];
+            sprintf(cmd, "./save.sh %d %d", data[1], temp_int_rec);
+            system(cmd);
+
+            while (has_more_data) {
+                uint8_t buff[] = { 0 };
+                has_more_data = radio.read(buff, 1);
+                printf("\n[%d]", buff[0]);
+            }
+        }
     }
 
     fflush(stdout);
