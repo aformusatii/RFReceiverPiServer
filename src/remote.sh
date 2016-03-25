892f9958389c4f2b0a777832524bf6660069238e
bender <aformusatii@gmail.com>
2016-03-25 11:30:57 +0200

    updated configuration.

--- src/remote.sh
+++ src/remote.sh
@@ -1,18 +1,29 @@
+
 case $1 in
 	24) mpc volume +1 > /dev/null 2>&1;;
 	25) mpc volume -1 > /dev/null 2>&1;;
 	20) mpc stop > /dev/null 2>&1;;
-	21) mpc play > /dev/null 2>&1;;
+	21) mpc play $(cat key_buf.txt);echo -n '' > key_buf.txt;;
 	23) mpc pause > /dev/null 2>&1;;
 	27) mpc next > /dev/null 2>&1;;
 	28) mpc prev > /dev/null 2>&1;;
 	18) mpc seek -1% > /dev/null 2>&1;;
 	19) mpc seek +1% > /dev/null 2>&1;;
-	10) echo "ten" ;;
-         6) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 1 1 1;;
-         5) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 255 255 255;;
-         4) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 50 5 0;;
+        30) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 1 1 1;;
+        31) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 255 255 255;;
+        32) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 50 5 0;;
          3) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 0 0 0;;
+        14) echo -n '0' >> key_buf.txt;;
+         4) echo -n '1' >> key_buf.txt;;
+         5) echo -n '2' >> key_buf.txt;;
+         6) echo -n '3' >> key_buf.txt;;
+         7) echo -n '4' >> key_buf.txt;;
+         8) echo -n '5' >> key_buf.txt;;
+         9) echo -n '6' >> key_buf.txt;;
+        10) echo -n '7' >> key_buf.txt;;
+        11) echo -n '8' >> key_buf.txt;;
+        12) echo -n '9' >> key_buf.txt;;
+        13) echo -n '' > key_buf.txt;;
 	*) echo "INVALID NUMBER!" ;;
 esac
 
--- src/start.sh
+++ src/start.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
 sudo ./receiver > receiver.log 2>&1 &
 ps aux | grep receiver
 echo Started NRF24L01 receiver.
