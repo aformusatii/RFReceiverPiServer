b4c13dbbe6daeef63a4ad97c3c042df5dbd89d3b
bender <aformusatii@gmail.com>
2016-03-27 23:30:11 +0300

    Updated remote commands.

--- src/remote.sh
+++ src/remote.sh
@@ -1,7 +1,7 @@
 
 case $1 in
-	24) mpc volume +1 > /dev/null 2>&1;;
-	25) mpc volume -1 > /dev/null 2>&1;;
+	24) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 116 100 0;;
+	25) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 116 101 0;;
 	20) mpc stop > /dev/null 2>&1;;
 	21) mpc play $(cat key_buf.txt);echo -n '' > key_buf.txt;;
 	23) mpc pause > /dev/null 2>&1;;
@@ -9,10 +9,10 @@ case $1 in
 	28) mpc prev > /dev/null 2>&1;;
 	18) mpc seek -1% > /dev/null 2>&1;;
 	19) mpc seek +1% > /dev/null 2>&1;;
-        30) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 1 1 1;;
-        31) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 255 255 255;;
-        32) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 50 5 0;;
-         3) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 0 0 0;;
+        30) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 115 1 1 1;;
+        31) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 115 255 255 255;;
+        32) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 115 50 5 0;;
+         3) /home/pi/repository/PiSocketClient/src/socketClient 192.168.1.120 115 0 0 0;;
         14) echo -n '0' >> key_buf.txt;;
          4) echo -n '1' >> key_buf.txt;;
          5) echo -n '2' >> key_buf.txt;;
